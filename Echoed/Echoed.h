c53bea6c5cb6b52622e7ee799556b1ed076e49e7
Trevor Grady <tgrady118@gmail.com>
2024-10-12 22:47:48 +0200

    Initial Commit

---
+++ Echoed/Echoed.h
@@ -0,0 +1,18 @@
+//
+//  Echoed.h
+//  Echoed
+//
+//  Created by Trevor (personal) on 2024-10-12.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Echoed.
+FOUNDATION_EXPORT double EchoedVersionNumber;
+
+//! Project version string for Echoed.
+FOUNDATION_EXPORT const unsigned char EchoedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Echoed/PublicHeader.h>
+
+
---
+++ EchoedTests/EchoedTests.swift
@@ -0,0 +1,17 @@
+//
+//  EchoedTests.swift
+//  EchoedTests
+//
+//  Created by Trevor (personal) on 2024-10-12.
+//
+
+import Testing
+@testable import Echoed
+
+struct EchoedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
